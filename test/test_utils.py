aae714fe8a8910908151968a2d51c6ad9cbc1237
shubham <shubhamkjaiswal40@gmail.com>
2024-04-26 20:34:48 +0530

    s3_mock

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,24 +1,54 @@
 import sys
 sys.path.insert(0, '../lambda_scripts')
-from utils import s3_writer,validator,output_file_formatter
+from utils import validator,output_file_formatter,s3_writer
 import pytest
+from moto import mock_aws
+import boto3
 
 
-input_file="/lambda_scripts/hello/_.csv"
-content='''transaction_id,automobile_name,order_date,booked_price
-2345,,,22333
-'''
+@pytest.fixture
+def s3_boto():
+    s3 = boto3.client('s3',region_name='us-east-1')
+    return s3
 
-def test_output_alias():
+@pytest.fixture
+def params():
+    para={}
+    para['input_file'] = "/lambda_scripts/hello/_.csv"
+    para['content'] ='''transaction_id,automobile_name,order_date,booked_price\n2345,,,22333'''
+    return para
+
+
+def test_output_alias(params):
+    input_file = params['input_file']
     valid_key,corrupt_key = output_file_formatter(input_file)
 
     assert valid_key == "valid_batch_files/__validrecords.csv"
     assert corrupt_key == "corrupt_batch_files/__corruptrecords.csv","Invalid_alias"
 
-def test_validator():
-    v,c,cr = validator(content)
-    #assert '2345,,,22333' in v
-    assert "transaction_id,automobile_name,order_date,booked_price" in c,"Header not appended"
-    assert '2345,,,22333' in c
-    assert '2345' in c
-    assert cr == 1
+def test_validator(params):
+    content = params['content']
+    valid,corrupt,corrupt_count = validator(content)
+    
+    assert "transaction_id,automobile_name,order_date,booked_price" in corrupt,"Header not appended"
+    assert '2345,,,22333' in corrupt
+    assert '2345' in corrupt
+    assert corrupt_count == 1
+
+@mock_aws
+def test_s3(s3_boto):
+    bucket = "auto-salesfiltered-batch2024"
+    key1 = "validkey"
+    key2 = "corruptkey"
+    body = "Content of body"
+
+    #create output s3 bucket as s3_writer requires.
+
+    s3_boto.create_bucket(Bucket=bucket)
+    s3_writer(s3_boto,validoutput_s3_key=key1,corruptoutput_s3_key=key2,corrupt_row=1 ,valid = body ,corrupt = body)
+    response = s3_boto.get_object(Bucket=bucket, Key=key1)
+    valid_body = response['Body'].read().decode('utf-8')
+    result = s3_boto.list_buckets()
+    assert "Content of body" == valid_body
+    assert "auto-salesfiltered-batch2024" in (result["Buckets"][0]["Name"])
+    
